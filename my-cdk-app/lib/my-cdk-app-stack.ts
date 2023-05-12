e7d27ee24a063358d2eadf5702e00c8a749a8267
Alex Madrazo <alex.madrazo@bgamestudios.com>
2023-05-12 13:16:07 +0200

    added jenkins container and networking rules

--- my-cdk-app/lib/my-cdk-app-stack.ts
+++ my-cdk-app/lib/my-cdk-app-stack.ts
@@ -6,42 +6,32 @@ export class MyCdkAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 2 });
+    const vpc = new ec2.Vpc(this, 'VPC');
 
-    const installScript = `#!/bin/bash
-    sudo yum update -y
-    sudo yum install -y java-1.8.0
-    sudo yum remove -y java-1.7.0-openjdk
-    sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-    sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-    sudo yum install -y jenkins
-    sudo service jenkins start
-    `;
+    const userdata = ec2.UserData.forLinux();
+    userdata.addCommands(
+      "yum update -y",
+      "yum install -y docker",
+      "service docker start",
+      "docker run -d -p 8080:8080 -p 50000:50000 jenkins/jenkins"
+    );
 
-    const scriptSsmParameter = new ssm.StringParameter(this, 'ScriptParameter', {
-      parameterName: '/scripts/jenkins-install',
-      stringValue: installScript,
-    });
-
-    const linuxAmi = new ec2.AmazonLinuxImage({
-      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-    });
-
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
-      vpc,
-      description: 'Allow ssh access to ec2 instances',
-      allowAllOutbound: true,
-    });
-
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8080), 'Allow Jenkins access from anywhere');
-
-    new ec2.Instance(this, 'Instance', {
+    const instance = new ec2.Instance(this, 'Instance', {
       vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: linuxAmi,
-      userData: ec2.UserData.custom(`sudo aws ssm get-parameter --name ${scriptSsmParameter.parameterName} --with-decryption --region ${this.region} --query Parameter.Value --output text | bash -`),
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
-      securityGroup: securityGroup,
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyName: "techub-master",  // replace this with your key pair name
+      userData: userdata
     });
+
+    const jenkinsPort = new ec2.Port({
+      protocol: ec2.Protocol.TCP,
+      fromPort: 8080,
+      toPort: 8080,
+      stringRepresentation: 'Allow Jenkins'
+    });
+
+    instance.connections.allowFromAnyIpv4(jenkinsPort, 'Allow Jenkins Port');
   }
 }
+
